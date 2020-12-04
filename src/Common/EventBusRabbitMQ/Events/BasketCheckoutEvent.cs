a339d0e006225c7f0ebcf9b24d8ea63a630f49db
okanyorukoglu <okan_yorukoglu@hotmail.com>
2020-12-04 12:07:56 +0300

    Add project files.

---
+++ src/Common/EventBusRabbitMQ/Events/BasketCheckoutEvent.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventBusRabbitMQ.Events
+{
+    class BasketCheckoutEvent
+    {
+        public Guid RequestId { get; set; }
+        public string UserName { get; set; }
+        public decimal TotalPrice { get; set; }
+
+        //Billing Adress
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAdress { get; set; }
+        public string AdressLine { get; set; }
+        public string Country { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+
+        //Payment
+        public string CardName { get; set; }
+        public string CardNumber { get; set; }
+        public string Expiration { get; set; }
+        public string CVV { get; set; }
+        public int PaymentMethod { get; set; }
+
+
+
+
+
+
+    }
+}
---
+++ src/Common/EventBusRabbitMQ/Producer/EventBusRabbitMQProducer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Unicode;
+using EventBusRabbitMQ.Events;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+
+namespace EventBusRabbitMQ.Producer
+{
+    public class EventBusRabbitMQProducer
+    {
+        private readonly IRabbitMQConnection _connection;
+
+        public EventBusRabbitMQProducer(IRabbitMQConnection connection)
+        {
+            _connection = connection ?? throw new ArgumentNullException(nameof(connection));
+        }
+
+        private void PublishBasketCheckout(string queueName, BasketCheckoutEvent publishModel)
+        {
+            using (var channel = _connection.CreateModel())
+            {
+                channel.QueueDeclare(queue:queueName, false, false, false, null);
+                var message = JsonConvert.SerializeObject(publishModel);
+                var body = Encoding.UTF8.GetBytes(message);
+
+
+                IBasicProperties properties = channel.CreateBasicProperties();
+                properties.Persistent = true;
+                properties.DeliveryMode = 2;
+
+                channel.ConfirmSelect();
+                channel.BasicPublish("",queueName,true,properties,body);
+                channel.WaitForConfirmsOrDie();
+
+                channel.BasicAcks += (sender, eventArgs) =>
+                {
+                    Console.WriteLine("Sent RabbitMQ");
+                };
+                channel.ConfirmSelect();
+
+            }
+        }
+    }
+}
